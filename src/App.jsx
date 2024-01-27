8498be62ababb3a737e91aa9450194a9162d13d6
RAJATS SHARMA <108745640+rajat-sha7@users.noreply.github.com>
2024-01-27 21:18:46 +0530

    first commchgfnfitvhgfhn

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from "./components/footer/Footer.jsx";
 import Home from "./pages/home/home.jsx";
 import Details from "./pages/details/details.jsx";
 import SearchResult from "./pages/serachResult/SearchResult.jsx";
-import Explore from "./pages/explore/explore.jsx";
+import Explore from "./pages/explore/Explore.jsx";
 
 
 import pageNotFound from "./pages/404/pageNotFound.jsx";
