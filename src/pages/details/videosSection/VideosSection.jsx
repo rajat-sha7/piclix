44505eeb0b24894a52dad8f8db25a3b56fe7c21b
RAJATS SHARMA <108745640+rajat-sha7@users.noreply.github.com>
2024-01-27 21:34:00 +0530

    first cojkj

--- src/pages/details/videosSection/VideosSection.jsx
+++ src/pages/details/videosSection/VideosSection.jsx
@@ -5,7 +5,8 @@ import "./style.scss";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import { PlayIcon } from "../PlayIcon";
 import VideoPopup from "../../../components/videoPopup/VideoPopup";
-import Img from "../../../components/lazyLoadImage/Img";
+
+import Img from "../../../components/LazyloadImage/img";
 
 const VideosSection = ({ data, loading }) => {
   const [show, setShow] = useState(false);
